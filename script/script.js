349284122be0680fea35dd7a756adb42de2d7943
EfraimNascimento <60748643+EfraimNascimento@users.noreply.github.com>
2026-02-02 12:49:41 -0300

    Mode escuro e smooth scroll

--- script/script.js
+++ script/script.js
@@ -2,12 +2,54 @@ function hamburguer(){
     const listmenu = document.querySelector('#menu ul');
     const menu = document.querySelector('#menu');
 
-    if(listmenu.style.transform == 'translateY(-185%)'){
-        listmenu.style.transform = 'translateY(0%)';
+    if(listmenu.style.display == 'none'){
+        hamburguerIconChange()
+        listmenu.style.display = 'block';
         menu.style.height = 'fit-content';
     }else{
-        listmenu.style.transform = 'translateY(-185%)';
-        menu.style.height = '95px';
+        hamburguerIconChange()
+        listmenu.style.display = 'none';
+        menu.style.height = '50px';
     }      
     
 };
+
+function hamburguerIconChange(){
+    const spans = document.querySelectorAll('#hamburguer span');
+    if(spans[0].style.display == 'block'){
+        spans[0].style.display = 'none';
+        spans[1].style.transform = 'translate(0px, 3px) rotate(-50deg)';
+        spans[2].style.transform = 'translate(0px, -3px) rotate(50deg)';
+    
+    }else{
+        spans[0].style.display = 'block';
+        spans[1].style.transform = 'rotate(0deg)';
+        spans[2].style.transform = 'rotate(0deg)';
+    }
+}
+
+const links = document.querySelectorAll('#menu ul li a');
+links.forEach(link => {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+
+        hamburguer();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if(target){
+            const headerHeight = document.querySelector('header').offsetHeight;
+            const targetPosition = target.offsetTop - headerHeight - 150;
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+
+});
+
+function darkMode(){
+    let sol = document.querySelector('body');
+    sol.classList.toggle('escuro');
+}
